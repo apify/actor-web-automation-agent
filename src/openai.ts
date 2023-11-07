825e29ebed98e6c9c7c4b0f72e7e06c0a6313da4
drobnikj <drobnik.j@gmail.com>
2023-11-07 13:53:34 +0100

    feat: new openAI model

--- src/openai.ts
+++ src/openai.ts
@@ -43,4 +43,13 @@ export const GPT_MODEL_LIST: {[key: string]: GPTModelConfig} = {
             output: 0.12,
         },
     },
+    'gpt-4-1106-preview': {
+        model: 'gpt-4-128k',
+        maxTokens: 128000,
+        interface: 'chat',
+        cost: {
+            input: 0.01,
+            output: 0.03,
+        },
+    },
 };
