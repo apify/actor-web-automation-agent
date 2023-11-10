137e067b8131aa3db0d125e3575f82bf2656e588
Jakub Drobník <drobnik.j@gmail.com>
2023-11-10 17:26:46 +0100

    fix: improve shrinking HTML (#15)

--- src/shrink_html.ts
+++ src/shrink_html.ts
@@ -1,6 +1,6 @@
 import cheerio from 'cheerio';
 import { type Page } from 'puppeteer';
-import { WHITELIST_ATTRIBUTES_WEB_AUTOMATION, WHITELIST_TAGS_WEB_AUTOMATION } from './consts.js';
+import { WHITELIST_ATTRIBUTES_WEB_AUTOMATION, WHITELIST_TAGS_WEB_AUTOMATION, HTML_COMMENT_REGEX } from './consts.js';
 
 interface ShrinkHtmlOptions {
     whiteListTags: string[];
@@ -42,14 +42,35 @@ export async function shrinkHtml(page: Page, options: ShrinkHtmlOptions) {
             });
             element.attribs = attributes;
         } else {
-            // Keep the children and remove the element with its content
-            $element.before($element.children());
+            // NOTE: Keep the children and element in some case, but remove the element itself.
+            const children = $element.children();
+            const text = $element.children().remove().end().text();
+            // Replace the element with its children
+            // If the parent element is button or link, keep element text (usefully in case some strange element tag are used as children for buttons or links)
+            if (text) {
+                let isParentButtonOrLink = false;
+                let prev = $element;
+                // Check just 3 levels up
+                for (let i = 0; i < 3; i++) {
+                    const parent = prev.parent().first();
+                    if (!parent || parent.length === 0) break;
+                    if (parent.prop('tagName').toLocaleLowerCase() === 'a' || parent.prop('tagName').toLocaleLowerCase() === 'button') {
+                        isParentButtonOrLink = true;
+                        break;
+                    }
+                    prev = parent;
+                }
+                if (isParentButtonOrLink) $element.before(text);
+            }
+            $element.before(children);
+            // Remove the element
             $element.remove();
         }
     }
     return $.html()
         .replace(/\s{2,}/g, ' ') // remove extra spaces
-        .replace(/>\s+</g, '><'); // remove all spaces between tags;
+        .replace(/>\s+</g, '><') // remove all spaces between tags;
+        .replace(HTML_COMMENT_REGEX, ''); // Remove HTML comments (e.g. <!-- comment -->)
 }
 
 export async function shrinkHtmlForWebAutomation(page: Page) {
